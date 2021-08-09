562019745ad60b57262693ae9441aab46f28ad97
Jorge <chauxjorge@hotmail.com>
2021-08-09 00:27:07 -0500

    Fix Indentation

--- src/components/Bar.js
+++ src/components/Bar.js
@@ -9,7 +9,7 @@ class Bar extends React.Component {
       <div className="progress bar-size">
         <div className="progress-bar bg-success" role="progressbar" style={{width: totalPositives+"%"}} aria-valuenow={totalPositives} aria-valuemin="0" aria-valuemax={totalPositives}>{totalPositives}</div>
         <div className="progress-bar bg-warning" role="progressbar" style={{width: totalNegatives+"%"}} aria-valuenow={totalNegatives} aria-valuemin="0" aria-valuemax={totalNegatives}>{totalNegatives}</div>
-	  	</div>
+      </div>
     )
   }
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -7,79 +7,79 @@ import './card.css';
 
 
 class Card extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			positClick: false,
-			negatClick: false,
-			voteNow: "btn btn-outline-secondary rounded-0 disabled",
-			voted: false,
-			voteMsg: "Vote Now",
-			positVotes: this.props.positives,
-			negVotes: this.props.negatives
-		}
-	}	
-	
-	enableVoteP =() => {
-		this.setState({positClick: true})
-		this.setState({voteNow: "btn btn-outline-secondary rounded-0"})
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      positClick: false,
+      negatClick: false,
+      voteNow: "btn btn-outline-secondary rounded-0 disabled",
+      voted: false,
+      voteMsg: "Vote Now",
+      positVotes: this.props.positives,
+      negVotes: this.props.negatives
+    }
+  }	
+  
+  enableVoteP =() => {
+    this.setState({positClick: true})
+    this.setState({voteNow: "btn btn-outline-secondary rounded-0"})
+  }
 
-	enableVoteN = () => {
-		this.setState({negatClick: true})
-		this.setState({voteNow: "btn btn-outline-secondary rounded-0"});
+  enableVoteN = () => {
+    this.setState({negatClick: true})
+    this.setState({voteNow: "btn btn-outline-secondary rounded-0"});
 }
 
 changes = () => {
-	
-	if (this.state.positClick) {
-		this.setState({positVotes: this.state.positVotes + 1});
-		this.setState({positClick: false})
+  
+  if (this.state.positClick) {
+    this.setState({positVotes: this.state.positVotes + 1});
+    this.setState({positClick: false})
 
-	} else {
-		this.setState({negVotes: this.state.negVotes + 1});
-		this.setState({negatClick: false})
-	}
+  } else {
+    this.setState({negVotes: this.state.negVotes + 1});
+    this.setState({negatClick: false})
+  }
 
-	if(!this.state.voted) {
-		this.setState({voted: true});
-		this.setState({voteMsg: "Vote aAgain"});
-	} else {
-		this.setState({voted: false});
-		this.setState({voteNow: "btn btn-outline-secondary rounded-0 disabled"});
-		this.setState({voteMsg: "Vote Now"});
-	}	
+  if(!this.state.voted) {
+    this.setState({voted: true});
+    this.setState({voteMsg: "Vote aAgain"});
+  } else {
+    this.setState({voted: false});
+    this.setState({voteNow: "btn btn-outline-secondary rounded-0 disabled"});
+    this.setState({voteMsg: "Vote Now"});
+  }	
 }
 
-	render() {
-		const lastUp = this.props.lastUpdated;
-		const cate = this.props.category;
-		return (
-			<div className="card" style={{width: "18rem"}}>
-				<div className="card-body card__col ">
-					<img src={this.props.cImage} alt="im" className="card-img-top" />
-					<h5 className="card-title person__name">{this.props.name}</h5>
-					<p className="card-text person__desc">{this.props.description}</p>
+  render() {
+    const lastUp = this.props.lastUpdated;
+    const cate = this.props.category;
+    return (
+      <div className="card" style={{width: "18rem"}}>
+        <div className="card-body card__col ">
+          <img src={this.props.cImage} alt="im" className="card-img-top" />
+          <h5 className="card-title person__name">{this.props.name}</h5>
+          <p className="card-text person__desc">{this.props.description}</p>
 
-					<Eyebrow votedState={this.state.voted} voteAgainSt={this.state.voteAgain} lastUpdated={lastUp} category={cate}/>
-					<div className="vote_buttons">
-						<button onClick={this.enableVoteP} className="icon-button btn-outline-secondary" aria-label="thumbs up">
-							<img src={thumbsUp} alt="thumbs up" className="button"/>
-						</button>
-						<button onClick={this.enableVoteN} className="icon-button btn-outline-secondary" aria-label="thumbs down">
-							<img src={thumbsDown} alt="thumbs down" className="button"/>
-						</button>
-					
-						<button onClick={this.changes} className={this.state.voteNow + " vote-now"}>
-							{this.state.voteMsg}
-						</button>
-					</div>
-					<br />
-					<Bar totalPositives={this.state.positVotes} totalNegatives={this.state.negVotes}/>
-				</div>
-			</div>
-		)
-	}
+          <Eyebrow votedState={this.state.voted} voteAgainSt={this.state.voteAgain} lastUpdated={lastUp} category={cate}/>
+          <div className="vote_buttons">
+            <button onClick={this.enableVoteP} className="icon-button btn-outline-secondary" aria-label="thumbs up">
+              <img src={thumbsUp} alt="thumbs up" className="button"/>
+            </button>
+            <button onClick={this.enableVoteN} className="icon-button btn-outline-secondary" aria-label="thumbs down">
+              <img src={thumbsDown} alt="thumbs down" className="button"/>
+            </button>
+          
+            <button onClick={this.changes} className={this.state.voteNow + " vote-now"}>
+              {this.state.voteMsg}
+            </button>
+          </div>
+          <br />
+          <Bar totalPositives={this.state.positVotes} totalNegatives={this.state.negVotes}/>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default Card
